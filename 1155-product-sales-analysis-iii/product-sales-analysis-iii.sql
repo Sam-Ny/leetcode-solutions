4fd2f85daf6adf77d6e0575641d224d8ba50f1f3
Sameer Nayak <63563252+Sam-Ny@users.noreply.github.com>
2025-12-24 16:24:02 +0530

    Delete 1155-product-sales-analysis-iii directory

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++
@@ -1,3 +0,0 @@
-/* Write your PL/SQL query statement below */
-select product_id, year as first_year, sum(quantity) as quantity, price from sales where (product_id, year) in (select product_id, min(year)
- from sales group by product_id) group by product_id, year, price;
