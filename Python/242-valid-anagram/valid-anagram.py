f298834cf926de12d141b01dc4bf36fb12e5ed33
Sameer Nayak <63563252+Sam-Ny@users.noreply.github.com>
2026-04-05 19:10:40 +0530

    Time: 12 ms (54.66%) | Memory: 19.3 MB (96.67%) - LeetSync

---
+++ Python/242-valid-anagram/valid-anagram.py
@@ -0,0 +1,32 @@
+''' Solution 1 - Using Sorted - O(nlogn) '''
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if sorted(s) == sorted(t):
+#             return True
+#         else:
+#             return False
+
+''' Solution 2 - Using Counter - O(n) '''
+# from collections import Counter
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+#         return Counter(s) == Counter(t)
+
+''' Solution 3 - Without using build-in libraries'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        char_count = {}
+
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+        
+        for char in t:
+            if char not in char_count or char_count[char] == 0:
+                return False
+            char_count[char] -= 1
+        
+        return True
