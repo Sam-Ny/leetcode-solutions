48da1d568de3cfb36e2cbeee1075bc2a5ac75f1c
Sameer Nayak <63563252+Sam-Ny@users.noreply.github.com>
2026-09-24 14:22:45 +0530

    Time: 443 ms (40.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ SQL/3767-find-students-who-improved/find-students-who-improved.sql
@@ -0,0 +1,14 @@
+/* Write your PL/SQL query statement below */
+with cte as (
+    select 
+    student_id,
+    subject,
+    FIRST_VALUE(score) over (partition by student_id,subject order by exam_date) as first_score,
+    LAST_VALUE(score)  over (partition by student_id,subject  order by exam_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as latest_score
+    from 
+    scores
+)
+
+select distinct * from cte
+where first_score < latest_score
+order by student_id
