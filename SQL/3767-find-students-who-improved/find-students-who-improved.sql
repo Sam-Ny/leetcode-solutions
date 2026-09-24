96493c2730be2c1a0c78941be4eca49795984c8d
Sameer Nayak <63563252+Sam-Ny@users.noreply.github.com>
2026-09-24 14:29:30 +0530

    Time: 311 ms (75.50%) | Memory: 0B (100.00%) - LeetSync

--- SQL/3767-find-students-who-improved/find-students-who-improved.sql
+++ SQL/3767-find-students-who-improved/find-students-who-improved.sql
@@ -4,11 +4,12 @@ with cte as (
     student_id,
     subject,
     FIRST_VALUE(score) over (partition by student_id,subject order by exam_date) as first_score,
-    LAST_VALUE(score)  over (partition by student_id,subject  order by exam_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as latest_score
+    FIRST_VALUE(score)  over (partition by student_id,subject  order by exam_date desc) as latest_score
     from 
     scores
 )
 
-select distinct * from cte
+select student_id, subject, first_score, latest_score  from cte
 where first_score < latest_score
+group by student_id, subject, first_score, latest_score 
 order by student_id
